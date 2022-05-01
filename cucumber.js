a52cc0327fd13f5f28c6e9c8c8325599c91d6967
Millane <ts-fares.millane@rakuten.com>
2022-05-01 18:00:18 +0200

    create test folder

--- cucumber.js
+++ cucumber.js
@@ -2,8 +2,8 @@ const config = require("./config");
 require('dotenv').config();
 let console, htmlReport, jsonReport, publish;
 config.console ? console = '-f @cucumber/pretty-formatter' : console = '-f summary';
-config.htmlReport ? htmlReport = `--format html:cucumber-report-${process.env.DRIVER}.html ` : htmlReport = '';
-config.jsonReport ? jsonReport = `--format json:cucumber-report-${process.env.DRIVER}.json ` : jsonReport = '';
+config.htmlReport ? htmlReport = `--format html:reports/cucumber-report-${process.env.DRIVER}.html ` : htmlReport = '';
+config.jsonReport ? jsonReport = `--format json:reports/cucumber-report-${process.env.DRIVER}.json ` : jsonReport = '';
 config.publish ? publish = '--publish-quiet' : publish = '';
 
 module.exports = {
